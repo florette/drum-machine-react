4212136741418093c124a04dc75d46b15d1a8860
Flore Laforge <flaforge@squiz.net>
2023-03-26 12:44:44 +1100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import "normalize.css";
+import pads from "./pads";
+import "./App.css";
+
+function App() {
+    const [currentPad, setCurrentPad] = useState("");
+
+    const playSound = (el) => {
+        el.currentTime = 0;
+        el.play();
+        setCurrentPad(el.dataset.name);
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", (e) => {
+            playSound(document.getElementById(e.key.toUpperCase()));
+        });
+    }, []);
+
+    const handleClick = (e) => {
+        playSound(e.target.firstElementChild);
+    };
+
+    const drumpads = pads.map((k) => {
+        return (
+            <button
+                onClick={handleClick}
+                id={`pad${k.keyboard}`}
+                key={k.keyboard}
+                className="drum-pad button-19">
+                {k.keyboard}
+                <audio
+                    src={k.sound}
+                    data-name={k.name}
+                    className="clip"
+                    id={k.keyboard}
+                />
+            </button>
+        );
+    });
+    return (
+        <div className="app">
+            <div id="drum-machine">
+                <div className="pad">{drumpads}</div>
+                <div className="controls">
+                    <div className="pad-name">
+                        <p id="display">{currentPad}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pads.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        keyboard: "Q",
+        name: "Heater 1",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+        keyboard: "W",
+        name: "Heater 2",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+        keyboard: "E",
+        name: "Heater 3",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+        keyboard: "A",
+        name: "Heater 4",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+        keyboard: "S",
+        name: "Clap",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+        keyboard: "D",
+        name: "Open-HH",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+        keyboard: "Z",
+        name: "Kick-n'-Hat",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+        keyboard: "X",
+        name: "Kick",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+        keyboard: "C",
+        name: "Closed-HH",
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+];
